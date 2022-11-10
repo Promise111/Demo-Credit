6b043b15b63e4b9e8fe71b1d75880c26f053d07c
Promise <promiseihunna@gmail.com>
2022-11-10 13:22:11 +0100

    added 'api' to auth and account controller routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,12 +2,12 @@ import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto, SigninDto } from './dto';
 
-@Controller('auth')
+@Controller('api/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @HttpCode(HttpStatus.CREATED)
-  @Post('api/signup')
+  @Post('signup')
   signup(@Body() dto: AuthDto) {
     return this.authService.signup(dto);
   }
